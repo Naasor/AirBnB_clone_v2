a9e78f71a7f70127975a3e90b3fecdd4f96f02de
Naasor <amofasandra7@gmail.com>
2024-03-11 23:19:42 +0000

    still fixing

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,9 +2,7 @@
 # Fabfile to distribute an archive to a web server.
 
 import os
-import subprocess as sub
 from fabric.api import env, put, run
-from datetime import datetime
 
 env.user = "ubuntu"
 env.hosts = ["100.25.188.244", "34.229.55.60"]
@@ -17,34 +15,33 @@ def do_deploy(archive_path):
         archive_path (str): The path of the archive to distribute.
     """
 
-    if not os.path.exists(archive_path):
+     if os.path.isfile(archive_path) is False:
         return False
-    try:
-        # getting name of archive from archive_path
-        temp = str(archive_path).split("/")[-1]
-        name = temp.split(".")[0]
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
 
-        # placing the archive
-        sub.run(["scp", "{}".format(archive_path),
-                 "{}@{}:/tmp/".format(env.user, env.host)], stdin="yes")
-        # put(archive_path, "/tmp/")
-
-        # uncompressing...
-        # extraction path
-        extrPath = "/data/web_static/releases/{}/".format(name)
-        run("mkdir -p {}".format(extrPath))
-        run("tar -xzf /tmp/{} -C {}".format(temp, extrPath))
-        run("mv {}/web_static/* {}".format(extrPath, extrPath))
-        # removing extracted
-        run("rm /tmp/{}".format(temp))
-
-        # deletes the symbolic
-        run("rm -rf /data/web_static/current")
-
-        # new symbolic link
-        run("ln -s {} /data/web_static/current".format(extrPath))
-        print("New version deployed!")
-        return True
-    except Exception as nope:
-        print(nope)
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
         return False
+    if run("rm -rf /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("mkdir -p /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(file, name)).failed is True:
+        return False
+    if run("rm /tmp/{}".format(file)).failed is True:
+        return False
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/current").failed is True:
+        return False
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(name)).failed is True:
+        return False
+    return True
